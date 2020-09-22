d00feb17c657f55fa981e046c11adfef19f84760
Mariusz <urban.mariusz@icloud.com>
2020-09-22 21:11:07 +0200

    zrobiona sekcja czwarta

--- src/js/components/section2/TextAndPhotoForth.jsx
+++ src/js/components/section2/TextAndPhotoForth.jsx
@@ -1,41 +1,37 @@
 import React from "react";
 import "../../../sass/_textAndPhoto.scss";
-import CompHome from "../../../img/CompHome.png";
+import CompPerson2 from "../../../img/CompPerson2.png";
 
 export default function TextAndPhoto() {
   return (
     <div className="text-photo">
       <div className="photo">
-        <img className="img" src={CompHome} alt="computer" />
+        <img className="img" src={CompPerson2} alt="computer" />
       </div>
       <div className="text-desc">
         <div className="header-desc">
-          <h1>Potrzebujesz czegoś? Wystarczy poprosić.</h1>
+          <h1>Intuicyjnie i interaktywnie</h1>
         </div>
         <div className="about">
-          <p className='divide'>
-            Yoga S740 ma mikrofony dalekiego zasięgu i inteligentne asystentki
-            głosowe Alexę oraz Cortanę. Pomogą Ci złożyć zamówienie, ustawić
-            przypomnienie czy odtworzyć muzykę — wystarczy poprosić z odległości
-            do 4 metrów. To kolejny przykład przewagi, jaką daje Ci Yoga S740.
-          </p>
-          <p>
-            Aby uzyskać więcej informacji o Cortanie, kliknij <a href='/'>tutaj</a>, a w celu
-            uzyskania dodatkowych informacji o Alexie, kliknij <a href='/'>tutaj</a>.
+          <p className="divide">
+            Kamera na podczerwień ułatwia życie. Wystarczy, że się oddalisz, a
+            oglądany film zostanie automatycznie wstrzymany. Zawartość
+            wyświetlacza laptopa możesz przenieść na dodatkowy monitor ruchem
+            głowy. A to jeszcze nie wszystko. Laptop Yoga S740 reaguje na Twój
+            styl użytkowania, dlatego możesz skupić się na tym, co ważne.
           </p>
         </div>
       </div>
       <div className="text-desc">
         <div className="header-desc">
-          <h1>Mobilne kino</h1>
+          <h1>Zna Cię lepiej</h1>
         </div>
         <div className="about">
           <p>
-            Wyświetlacz o przekątnej 14 cali w wąskiej jak brzytwa ramce
-            prezentuje imponującą gamę kolorów i szczegółów. Ekran do poziomu 4K
-            VESA400 HDR z jasnością 500 nitów i spektakularną technologią Dolby
-            Vision™ oraz bogaty, wielowarstwowy dźwięk 3D z głośników z Dolby
-            Atmos® oferują niezwykle realistyczną i wciągającą rozrywkę.
+            Możesz na przykład logować się do niego samym uśmiechem. Dzięki
+            kamerze na podczerwień i funkcji rozpoznawania twarzy Windows Hello
+            możesz zacząć pracę trzy razy szybciej niż wpisując hasło. Jest też
+            świetnie zabezpieczony.
           </p>
         </div>
       </div>
